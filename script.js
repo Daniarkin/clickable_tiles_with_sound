e2d81b74be313ac75883a083f3fd1059c430edfa
daniarkin <jishikukuto@gmail.com>
2022-01-13 18:54:26 +0300

    audio on click

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const container = document.body.children[0]
+
+fillTiles()
+
+onresize = fillTiles
+
+
+
+container.onmouseover = event => {
+  if (event.target === container || event.target.className === 'row') return
+
+  event.target.style.background = getNextColor()
+}
+
+function fillTiles() {
+  let maxRows = Math.floor((innerHeight - 16) / 100)
+  let maxColumns = Math.floor((innerWidth - 16) / 100)
+
+  container.innerHTML = `
+    <div class="row">${'<button>Boop Me</button>'.repeat(maxColumns)}</div>
+  `.repeat(maxRows)
+}
+
+function getNextColor() {
+  getNextColor.i = getNextColor.i || 0
+  return `hsl(${getNextColor.i++ * 10}, 65%, 70%)`
+
+}
+
+
+var boop = document.getElementById('boop'),
+  boopAudio = document.getElementById('boopAudo');
+boop.addEventListener('click', fPlay, false);
+function fPlay() {
+  boopAudio.play();
+}
